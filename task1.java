4602852cc6da474e6828ab7c11bd732d1bc7cfbd
sakshi kingare <kingaresakshi@gmail.com>
2024-01-09 13:24:18 +0530

    task1

---
+++ task1.java
@@ -0,0 +1,65 @@
+package codsoft;
+
+
+import java.util.Random;
+import java.util.Scanner;
+
+class NumberGuessingGame {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        Random random = new Random();
+
+        int lowerBound = 1;
+        int upperBound = 100;
+        int maxAttempts = 7;
+        int rounds = 0;
+        int totalScore = 0;
+
+        System.out.println("Welcome to the Number Guessing Game!");
+
+        do {
+            int targetNumber = random.nextInt(upperBound - lowerBound + 1) + lowerBound;
+            int attempts = 0;
+            int userGuess;
+
+            System.out.println("\nRound " + (rounds + 1) + ": Guess the number between " + lowerBound + " and " + upperBound);
+
+            while (attempts < maxAttempts) {
+                System.out.print("Enter your guess: ");
+                userGuess = scanner.nextInt();
+                attempts++;
+
+                if (userGuess == targetNumber) {
+                    System.out.println("Congratulations! You guessed the number in " + attempts + " attempts.");
+                    totalScore += maxAttempts - attempts + 1;
+                    break;
+                } else if (userGuess < targetNumber) {
+                    System.out.println("Too low! Try again.");
+                } else {
+                    System.out.println("Too high! Try again.");
+                }
+            }
+
+            if (attempts == maxAttempts) {
+                System.out.println("Sorry, you've reached the maximum number of attempts. The correct number was: " + targetNumber);
+            }
+
+            rounds++;
+
+            System.out.print("Do you want to play another round? (yes/no): ");
+            String playAgain = scanner.next().toLowerCase();
+
+            if (!playAgain.equals("yes")) {
+                break;
+            }
+
+        } while (true);
+
+        System.out.println("\nGame Over!");
+        System.out.println("Total Rounds Played: " + rounds);
+        System.out.println("Total Score: " + totalScore);
+
+        scanner.close();
+    }
+}
